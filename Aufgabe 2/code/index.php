1d520f326b41975ef3cbf70cf438dd7680b17080
EC2 Default User <ec2-user@ip-172-31-25-224.eu-central-1.compute.internal>
2021-09-09 15:05:40 +0000

    x-if

--- Aufgabe 2/code/index.php
+++ Aufgabe 2/code/index.php
@@ -40,8 +40,8 @@ $files = array_values(array_diff(scandir($pictures_path), array('.', '..')));
 	    <button @click="$refs.upload.click();">
 	        <svg style="filter: invert(100%)" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"><path d="M8 10h-5l9-10 9 10h-5v10h-8v-10zm11 9v3h-14v-3h-2v5h18v-5h-2z"/></svg>
 	    </button>
-	    <div x-show="current">
+	    <template x-if="current">
 	    	<img :src="'pictures/'+current" />
-	    </div>
+	    </template>
     </body>
 </html>
